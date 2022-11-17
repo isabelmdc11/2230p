37ae7da9e4fb65f63202bfd022a4c3d9e504bc3d
alumno <alumno@DESKTOP-UE84JL7>
2022-11-17 20:21:04 +0100

    comic

--- src/proyectogit2/MiniGit.java
+++ src/proyectogit2/MiniGit.java
@@ -3,7 +3,7 @@ package proyectogit2;
 public class MiniGit {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
+		System.out.println("hola");
 
 		
 		
